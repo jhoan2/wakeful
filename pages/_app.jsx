b6bf9b3c622691968551189690b8763cba3bb909
jhoan2 <johnhoang1994@gmail.com>
2023-11-17 12:19:29 -0800

    landing page redirects to home page if you've logged in

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,13 @@
 import '../styles/globals.css'
-
 import { CeramicWrapper } from "../context";
-
 import React, { useState, useEffect } from "react"
-
 import { useCeramicContext } from '../context';
-import { authenticateCeramic } from '../utils';
-import AuthPrompt from "../components/did-select-popup";
 import { ApolloClient, ApolloLink, InMemoryCache, Observable, ApolloProvider } from '@apollo/client'
 
 
 const MyApp = ({ Component, pageProps }) => {
   const clients = useCeramicContext()
   const { ceramic, composeClient } = clients
-  const [loggedIn, setLoggedIn] = useState(false)
   const link = new ApolloLink((operation) => {
     return new Observable((observer) => {
       composeClient.execute(operation.query, operation.variables).then(
@@ -30,37 +24,13 @@ const MyApp = ({ Component, pageProps }) => {
 
   const apolloClient = new ApolloClient({ cache: new InMemoryCache(), link })
 
-  const handleLogin = async () => {
-    await authenticateCeramic(ceramic, composeClient)
-  }
-
-  // Update to include refresh on auth
-  useEffect(() => {
-    if (localStorage.getItem('logged_in' === true)) {
-      handleLogin()
-    }
-
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-
-    const popup = document.getElementById('popup')
-    if (popup.style.display === "none") {
-      //change the logo here
-      setLoggedIn(true)
-    }
-  }, [])
-
   return (
     <div>
-      <AuthPrompt />
       <ApolloProvider client={apolloClient}>
         <div>
           <CeramicWrapper>
             <div>
-              <Component {...pageProps} loggedIn={loggedIn} />
+              <Component {...pageProps} />
             </div>
           </CeramicWrapper>
         </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,110 @@
-import { useEffect, useState } from 'react';
-import { useCeramicContext } from '../context';
-
-import Head from 'next/head'
-
-import AuthPrompt from "../components/did-select-popup";
-import React from "react";
-import { authenticateCeramic } from "../utils";
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Home = () => {
-  const clients = useCeramicContext()
-  const { ceramic, composeClient } = clients
+  const router = useRouter();
 
-  const handleLogout = () => {
-    localStorage.setItem("logged_in", "false")
-    localStorage.removeItem('ceramic:did_seed')
-    localStorage.removeItem('ceramic:eth_did')
-    localStorage.removeItem('did')
-    localStorage.removeItem('ceramic:auth_type')
-    console.log(localStorage.getItem('logged_in'))
-    window.location.reload();
-    console.log('logged out')
-  }
+  useEffect(() => {
+    if (localStorage.getItem('logged_in') === 'true') {
+      router.push('/home')
+    }
 
-  // useEffect(() => {
-  //   getPosts()
-  // }, [])
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+  }, [])
 
   return (
     <>
       <Head>
-        <title>Ceramic Social</title>
+        <title>Idealite</title>
         {/* TODO: UPDATE FAVICON */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick={() => handleLogout()}>Logout</button>
+      <div className="isolate bg-white">
+        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+          <svg
+            className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+            viewBox="0 0 1155 678"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+              fillOpacity=".3"
+              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+            />
+            <defs>
+              <linearGradient
+                id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                x1="1155.49"
+                x2="-78.208"
+                y1=".177"
+                y2="474.645"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#fde68a" />
+                <stop offset={1} stopColor="#fef08a" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+        <div className="px-6 pt-6 lg:px-8">
+          <div>
+            <nav className="flex h-9 items-center justify-between" aria-label="Global">
+              <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
+                <h2 className="text-3xl font-semibold text-gray-800 dark:text-white">idealite</h2>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <main>
+          <div className="relative px-6 lg:px-8">
+            <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+              <div>
+                <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+                  <svg
+                    className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+                    viewBox="0 0 1155 678"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+                      fillOpacity=".3"
+                      d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+                        x1="1155.49"
+                        x2="-78.208"
+                        y1=".177"
+                        y2="474.645"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="#fbbf24" />
+                        <stop offset={1} stopColor="#facc15" />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <button className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-orange-400 text-gray hover:bg-gradient-to-r from-amber-200 to-yellow-400 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Download the Chrome Extension</button>
+            <button onClick={() => router.push('/home')} className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-orange-400 text-gray hover:bg-gradient-to-r from-amber-200 to-yellow-400 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Enter the app</button>
+          </div>
+        </main>
+
+        {/* <div className='shadow-xl'>
+        <Image src='/icarus-notes.png' alt='Picture of notes page' width={500} height={500} />
+      </div> */}
+      </div>
     </>
   );
 }
