559a2895e995f2743983a775d427b6a31a5727f8
jhoan2 <johnhoang1994@gmail.com>
2023-11-20 20:13:06 -0800

    fixed destructuring of error message

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -3,8 +3,8 @@ import SideBar from './SideBar'
 import BottomNavBar from './BottomNavBar'
 import { useRouter } from 'next/router';
 
-export default function ErrorPage() {
-    const router = useRouter({ message });
+export default function ErrorPage({ message }) {
+    const router = useRouter();
 
     return (
         <div className='flex h-screen'>
@@ -12,7 +12,7 @@ export default function ErrorPage() {
             <div className='flex-grow flex justify-center items-center'>
                 <div className='flex flex-col items-center'>
                     <div className='rounded-full bg-gray-200 p-10 inline-flex justify-center items-center'>
-                        <img src={'error-blue-cat.png'} className="w-40 h-40 object-cover rounded-full" />
+                        <img src={'/error-blue-cat.png'} className="w-40 h-40 object-cover rounded-full" />
                     </div>
                     <p className="mt-4 text-2xl font-semibold text-gray-800">{message}</p>
                     <button onClick={() => router.push('/home')} className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-orange-400 text-gray hover:bg-gradient-to-r from-amber-200 to-yellow-400 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Refresh</button>
