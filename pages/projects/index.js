0bdc027fcf2ca718281bde9abf30cbe6a3aa8192
jhoan2 <johnhoang1994@gmail.com>
2023-12-04 12:27:21 -0800

    feat: projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,16 +2,31 @@ import React from 'react'
 import { useCeramicContext } from '../../context';
 import SideBar from '../../components/SideBar';
 import BottomNavBar from '../../components/BottomNavBar';
-
+import SkeletonHomeCard from '../../components/SkeletonHomeCard';
+import NoContent from '../../components/NoContent';
 export default function index() {
     const clients = useCeramicContext()
     const { composeClient, ceramic } = clients
-
+    const data = true
+    const loading = false
     return (
         <div className='flex h-screen'>
-            <SideBar />
-            <button>Click here </button>
-            <BottomNavBar />
+            <SideBar page={'projects'} />
+            {loading ?
+                (<div className='md:flex'>
+                    <SkeletonHomeCard />
+                    <SkeletonHomeCard />
+                    <SkeletonHomeCard />
+                </div>)
+                :
+                (
+                    data ?
+                        <div>Projects</div>
+                        :
+                        <NoContent src='/no-content-cat.png' />
+                )
+            }
+            <BottomNavBar page={'projects'} />
         </div>
     )
 }
