3f0c192575057003830d12a2bdc3f88ee03309c4
jhoan2 <johnhoang1994@gmail.com>
2023-12-04 16:23:45 -0800

    style: changed styles of buttons

--- components/ResourceCard.jsx
+++ components/ResourceCard.jsx
@@ -68,7 +68,7 @@ export default function ResourceCard({ card }) {
         console.log('show text area')
     }
 
-    console.log(deleteData, deleteError)
+    // console.log(deleteData, deleteError)
 
     return (
         <div className="m-3 flex flex-col bg-white border shadow-sm max-w-sm rounded-xl group hover:shadow-lg transition p-6 dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
@@ -86,11 +86,11 @@ export default function ResourceCard({ card }) {
                     Updated November 7, 2022
                 </p>
                 <div className='space-x-3'>
-                    <button className='hover:bg-gray-300 rounded-xl' title='Add an image'>
+                    <button className='py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600' title='Add an image'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='w-6 h-6'><path d="M2.9918 21C2.44405 21 2 20.5551 2 20.0066V3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918ZM20 15V5H4V19L14 9L20 15ZM20 17.8284L14 11.8284L6.82843 19H20V17.8284ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path></svg>
                     </button>
                     {editorStateChanged ?
-                        <button>
+                        <button title='Submit Changes' className='py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'>
                             Submit
                         </button> :
                         null
