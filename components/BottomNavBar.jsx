311b8a30d79948f074bf4a6caf796be097e52d2a
jhoan2 <johnhoang1994@gmail.com>
2023-12-06 10:09:06 -0800

    refactor: conditional rendering of add note button

--- components/BottomNavBar.jsx
+++ components/BottomNavBar.jsx
@@ -16,6 +16,15 @@ export default function BottomNavBar({ page }) {
                     </button>
                     <span className="text-xs">Projects</span>
                 </Link>
+                {(page === 'resource') ?
+                    <div className="flex flex-col items-center">
+                        <button title='Open Add Note' onClick={() => setShowModal(true)} className={`hs-tooltip-toggle w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 ${page === 'projects' ? 'bg-gray-400' : 'bg-slate-100'}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='w-10 h-10'><path d="M11 11V7H13V11H17V13H13V17H11V13H7V11H11ZM12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"></path></svg>
+                        </button>
+                    </div>
+                    :
+                    null
+                }
                 <a href="#" className="flex flex-col items-center">
                     <svg className="h-6 w-6" /* Add your SVG icon here */></svg>
                     <span className="text-xs">Notifications</span>
