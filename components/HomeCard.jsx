c911b76d9a01af7de89e64de93e50947e7d13404
jhoan2 <johnhoang1994@gmail.com>
2023-12-09 11:43:19 -0800

    refactor(homeCard): changed to pinata link and added cid ternary

--- components/HomeCard.jsx
+++ components/HomeCard.jsx
@@ -4,15 +4,30 @@ export default function HomeCard({ resource }) {
     const { id, title, cid } = resource.node.resource
     return (
         <Link href={`/resource/${id}`} className="m-3 flex flex-col bg-white border shadow-sm max-w-sm rounded-xl group hover:shadow-lg transition dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            <div className="relative pt-[50%]  rounded-t-xl overflow-hidden">
-                <img className="w-full h-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                    src={`https://${cid}.ipfs.w3s.link`}
-                    onError={({ currentTarget }) => {
-                        currentTarget.onerror = null; //prevent looping
-                        currentTarget.src = '/placeholder-img.png'
-                    }}
-                    alt="Image Description" />
-            </div>
+            {!cid ?
+                <div className="relative pt-[50%] rounded-t-xl overflow-hidden">
+                    <div className="w-full h-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+                        style={{
+                            backgroundImage: 'linear-gradient(to right, #ff9966, #ff5e62)',
+                            backgroundSize: 'cover'
+                        }}
+                    >
+                    </div>
+                </div>
+                :
+                <div className="relative pt-[50%]  rounded-t-xl overflow-hidden">
+                    <img className="w-full h-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+                        src={`${process.env.NEXT_PUBLIC_GATEWAY}/${cid}`}
+                        onError={({ currentTarget }) => {
+                            currentTarget.onerror = null; //prevent looping
+                            currentTarget.src = '/placeholder-img.png'
+                        }}
+                        crossOrigin='anonymous'
+                        alt="Image Description" />
+                </div>
+            }
+
+
             <div className="p-4 md:p-5">
                 <h3 className="text-lg font-bold text-gray-800 dark:text-white">
                     {title}
