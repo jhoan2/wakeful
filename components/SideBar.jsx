685f84999aa0595d55bf09516a7954e0abc38307
jhoan2 <johnhoang1994@gmail.com>
2023-12-16 18:11:15 -0800

    refactor(side bar): changed logo

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import AuthPrompt from './did-select-popup'
 import { useCeramicContext } from '../context'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 export default function SideBar({ page }) {
     const clients = useCeramicContext();
@@ -18,10 +19,10 @@ export default function SideBar({ page }) {
     }
 
     return (
-        <div id="sidebar-mini" className="flex flex-col hs-overlay hs-overlay-open:translate-x-0 -translate-x-full h-full transition-all duration-300 transform hidden z-[60] bg-slate-100 border-e border-gray-200 md:block md:translate-x-0 md:end-auto md:bottom-0  dark:bg-gray-800 dark:border-gray-700">
+        <div id="sidebar-mini" className="flex flex-col hs-overlay hs-overlay-open:translate-x-0 -translate-x-full h-full transition-all duration-300 transform hidden z-[1] bg-slate-100 border-e border-gray-200 md:block md:translate-x-0 md:end-auto md:bottom-0  dark:bg-gray-800 dark:border-gray-700">
             <div className="flex flex-col justify-center items-center gap-y-2 py-4">
                 <div className="mb-4">
-                    <img src='/idealite-day-double.svg' alt='idealite logo' className='w-16 h-16' />
+                    <Image src='/icon128.png' alt='idealite logo' width={64} height={64} />
                 </div>
                 <div className="hs-tooltip inline-block [--placement:right]">
                     <Link href={'/home'}>
@@ -31,13 +32,13 @@ export default function SideBar({ page }) {
                     </Link>
                 </div>
 
-                <div className="hs-tooltip inline-block [--placement:right]">
+                {/* <div className="hs-tooltip inline-block [--placement:right]">
                     <Link href={'/projects'}>
                         <button title='Projects' className={`hs-tooltip-toggle  w-16 h-16 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-500 hover:bg-gray-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 ${page === 'projects' ? 'bg-gray-400' : 'bg-slate-100'}`}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='w-10 h-10'><path d="M12.4142 5H21C21.5523 5 22 5.44772 22 6V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H10.4142L12.4142 5ZM8 19H20V11H8V19ZM6 19V10C6 9.44772 6.44772 9 7 9H20V7H11.5858L9.58579 5H4V19H6Z"></path></svg>
                         </button>
                     </Link>
-                </div>
+                </div> */}
 
                 <div className="hs-tooltip inline-block [--placement:right]">
                     {ceramic.did ?
