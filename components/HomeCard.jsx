cf0f0f73020aca2cd7101603a9cc5c7350d41f06
jhoan2 <johnhoang1994@gmail.com>
2023-12-20 20:04:52 -0800

    style(HomeCard): added min width and max width

--- components/HomeCard.jsx
+++ components/HomeCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 export default function HomeCard({ resource }) {
-    const { id, title, cid } = resource.node.resource
+    const { id, title, cid, updatedAt } = resource.node.resource
+    const localeUpdatedAt = new Date(updatedAt).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    })
     return (
-        <Link href={`/resource/${id}`} className="m-3 flex flex-col bg-white border shadow-sm max-w-sm rounded-xl group hover:shadow-lg transition dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+        <Link href={`/resource/${id}`} className="m-3 flex flex-col bg-white border shadow-sm rounded-xl min-w-[330px] max-w-[330px] group hover:shadow-lg transition dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
             {!cid ?
                 <div className="relative pt-[50%] rounded-t-xl overflow-hidden">
                     <div className="w-full h-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
@@ -32,11 +37,8 @@ export default function HomeCard({ resource }) {
                 <h3 className="text-lg font-bold text-gray-800 dark:text-white">
                     {title}
                 </h3>
-                <p className="mt-1 text-gray-500 dark:text-gray-400">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                </p>
                 <p className="mt-5 text-xs text-gray-500 dark:text-gray-500">
-                    Last updated 5 mins ago
+                    Updated at: {localeUpdatedAt}
                 </p>
             </div>
         </Link>
