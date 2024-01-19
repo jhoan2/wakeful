0f4e44652337c3d5db153dac21afae8cf8472fc5
jhoan2 <johnhoang1994@gmail.com>
2024-01-19 11:46:56 -0800

    cleanup: code

--- components/project/ProjectSidePanel.jsx
+++ components/project/ProjectSidePanel.jsx
@@ -21,12 +21,9 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { gql, useMutation } from '@apollo/client';
 import {
     Dialog,
-    DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
 } from "@/components/ui/dialog";
 
 
