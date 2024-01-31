02eaa24ea0e8272d76e3354f08faee13f03638b9
jhoan2 <johnhoang1994@gmail.com>
2024-01-30 20:17:03 -0800

    feat(profile): installed shadcn-ui components

---
+++ @/components/ui/avatar.jsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
+
+import { cn } from "@/lib/utils"
+
+const Avatar = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn(
+      "-twrelative -twflex -twh-10 -tww-10 -twshrink-0 -twoverflow-hidden -twrounded-full",
+      className
+    )}
+    {...props} />
+))
+Avatar.displayName = AvatarPrimitive.Root.displayName
+
+const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn("-twaspect-square -twh-full -tww-full", className)}
+    {...props} />
+))
+AvatarImage.displayName = AvatarPrimitive.Image.displayName
+
+const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      "-twflex -twh-full -tww-full -twitems-center -twjustify-center -twrounded-full -twbg-slate-100 dark:-twbg-slate-800",
+      className
+    )}
+    {...props} />
+))
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+
+export { Avatar, AvatarImage, AvatarFallback }
