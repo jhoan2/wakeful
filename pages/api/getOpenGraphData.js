83403a80383351cc0e316fc85c4e82fcbd8fb152
jhoan2 <johnhoang1994@gmail.com>
2024-05-12 14:38:26 -0700

    feat(add notes on web): added api endpoint to get og data

---
+++ pages/api/getOpenGraphData.js
@@ -0,0 +1,21 @@
+const ogs = require('open-graph-scraper');
+
+const handler = async (req, res) => {
+
+    switch (req.method) {
+        case 'GET':
+            const { url } = req.query
+            try {
+                const { result, error } = await ogs({ url: url });
+                if (error) return res.status(500).send({ message: error })
+                return res.status(200).send({ result: result })
+            } catch (error) {
+                return res.status(500).send({ message: error.message })
+            }
+        default:
+            res.setHeader('Allow', ['GET'])
+            res.status(405).end(`Method ${req.method} is not allowed.`)
+    }
+}
+
+export default handler 
