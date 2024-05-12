368fe6eb5e12154237aa85bd99ac7bf983440660
jhoan2 <johnhoang1994@gmail.com>
2024-05-12 14:53:22 -0700

    feat(home add resource): only show ogData if there is the data

--- components/home/HomeCreateOpenGraph.jsx
+++ components/home/HomeCreateOpenGraph.jsx
@@ -16,7 +16,7 @@ export default function HomeCreateOpenGraph({ url }) {
     const { composeClient } = clients
     const [loadingSubmitAddResource, setLoadingSubmitAddResource] = useState(false)
     const [loadingOgData, setLoadingOgData] = useState(false)
-    const [ogData, setOgData] = useState({})
+    const [ogData, setOgData] = useState(null)
 
     const CREATE_ACCOUNT_RESOURCE = gql`
     mutation createAccountResource($input: CreateIdealiteAccountResourcesInput!) {
@@ -115,44 +115,45 @@ export default function HomeCreateOpenGraph({ url }) {
                 loadingOgData ?
                     <p className='flex justify-center'>Gathering url details...</p>
                     :
-                    <div>
-                        <Card>
-                            <CardHeader>
+                    (ogData &&
+                        <div>
+                            <Card>
+                                <CardHeader>
+                                    {
+                                        (ogData.ogImage?.length > 0) ?
+                                            <img
+                                                alt="OG Image"
+                                                className="w-full h-[200px] object-cover rounded-t-lg"
+                                                height="200"
+                                                src={`${ogData.ogImage[0].url}`}
+                                                style={{
+                                                    aspectRatio: "400/200",
+                                                    objectFit: "cover",
+                                                }}
+                                                width="400"
+                                            />
+                                            :
+                                            null
+                                    }
+                                </CardHeader>
+                                <CardContent>
+                                    <CardTitle>{ogData.ogTitle}</CardTitle>
+                                    <CardDescription>{ogData.ogDescription}</CardDescription>
+                                    <div className='flex justify-between items-center'>
+                                        <CardDescription>Author: {ogData.author}</CardDescription>
+                                        <CardDescription>Type: {ogData.ogType}</CardDescription>
+                                    </div>
+                                </CardContent>
+                            </Card>
+                            <div className='flex justify-end w-full pt-4'>
                                 {
-                                    (ogData.ogImage?.length > 0) ?
-                                        <img
-                                            alt="OG Image"
-                                            className="w-full h-[200px] object-cover rounded-t-lg"
-                                            height="200"
-                                            src={`${ogData.ogImage[0].url}`}
-                                            style={{
-                                                aspectRatio: "400/200",
-                                                objectFit: "cover",
-                                            }}
-                                            width="400"
-                                        />
+                                    loadingSubmitAddResource ?
+                                        <Button disabled={loadingSubmitAddResource} className='w-full'>Submitting...</Button>
                                         :
-                                        null
+                                        <Button onClick={() => createNewResource()} className='w-full' >Submit</Button>
                                 }
-                            </CardHeader>
-                            <CardContent>
-                                <CardTitle>{ogData.ogTitle}</CardTitle>
-                                <CardDescription>{ogData.ogDescription}</CardDescription>
-                                <div className='flex justify-between items-center'>
-                                    <CardDescription>Author: {ogData.author}</CardDescription>
-                                    <CardDescription>Type: {ogData.ogType}</CardDescription>
-                                </div>
-                            </CardContent>
-                        </Card>
-                        <div className='flex justify-end w-full pt-4'>
-                            {
-                                loadingSubmitAddResource ?
-                                    <Button disabled={loadingSubmitAddResource} className='w-full'>Submitting...</Button>
-                                    :
-                                    <Button onClick={() => createNewResource()} className='w-full' >Submit</Button>
-                            }
-                        </div>
-                    </div>
+                            </div>
+                        </div>)
             }
         </div>
     )
